c313d53075065c8f95ece2e5d6f8933e518b4001
abuhasy <yusra.abu.hasna@hva.nl>
2021-10-15 01:28:45 +0200

    forgot password component is fixed!

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './components/forms/login/login.component';
 import { RegisterComponent } from './components/forms/register/register.component';
+import { ForgotPasswordComponent } from './components/forms/forgot-password/forgot-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
+    ForgotPasswordComponent,
   ],
   imports: [
     BrowserModule,
