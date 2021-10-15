d0c3fad6b23701d50a7474cbadaf0ab055a1747a
ebrahia6 <anas.ebrahim@hva.nl>
2021-10-15 16:00:37 +0200

    fix

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,12 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
   ],
   imports: [
     BrowserModule,
