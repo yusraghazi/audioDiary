1f12260acf712371329815b8bb0a09afdc481647
abuhasy <yusra.abu.hasna@hva.nl>
2021-10-17 15:08:56 +0200

    start comments view

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from './components/forms/login/login.component';
 import { RegisterComponent } from './components/forms/register/register.component';
 import { ForgotPasswordComponent } from './components/forms/forgot-password/forgot-password.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CommentsComponent } from './components/forms/comments/comments.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +19,7 @@ import { HeaderComponent } from './components/header/header.component';
     LoginComponent,
     RegisterComponent,
     ForgotPasswordComponent,
+    CommentsComponent,
   ],
   imports: [
     BrowserModule,
