d1ac09ed7dff86a2f9c1403fc66daf1137bba426
anas ebrahim <anas.ebrahim@hva.nl>
2021-10-26 22:55:05 +0200

    re-make profile to fit the website style

--- app/src/app/components/profile/profile.component.ts
+++ app/src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Post} from "../../models/post";
+import {Theme} from "../../enums/theme";
 declare var $: any;
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,16 @@ declare var $: any;
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  posts: Post[];
+  text: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 
   constructor() { }
 
   ngOnInit(): void {
+    this.posts =[];
+    this.posts.push(new Post(1, "River Sounds", this.text,"river.jpg",Theme.SUN, true))
+    this.posts.push(new Post(3, "Amazon Birds", this.text,"amazon.jpg",Theme.FOREST, true))
+    this.posts.push(new Post(4, "Sound of Waves", this.text,"seawaves.jpg",Theme.WATER, false))
   }
 
 }
