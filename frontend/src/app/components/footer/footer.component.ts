69315f28da621e7f92c2f3ec13689a7c6b290973
Taner <tanerozguner@hotmail.com>
2021-11-25 00:56:53 +0100

    V1.07
    Added functionalities for recording

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgAudioRecorderService, OutputFormat} from 'ng-audio-recorder';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
 
-  constructor() { }
+
+
+export class FooterComponent implements OnInit {
+  value = './assets/img/rec-button.png'; //default_value
+  constructor(private audioRecorderService: NgAudioRecorderService) {
+    this.audioRecorderService.recorderError.subscribe(recorderErrorCase => {
+      // Handle Error
+    })
+
+  }
+
+  startRecording() {
+    this.audioRecorderService.startRecording();
+    console.log("recording")
+  }
+
+  stopRecording() {
+    this.audioRecorderService.stopRecording(OutputFormat.WEBM_BLOB).then((output) => {
+      // do post output steps
+      console.log("it works");
+    }).catch(errrorCase => {
+      console.log("error")
+      // Handle Error
+    });
+  }
+
+  // updateImage() {
+  //   this.value = 'https://www.pngall.com/wp-content/uploads/5/Pause-Button-Transparent.png';
+  // }
+
 
   ngOnInit(): void {
+
   }
 
 }
