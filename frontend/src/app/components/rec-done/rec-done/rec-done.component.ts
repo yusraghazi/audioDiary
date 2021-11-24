e2d904ab7babeaca352fe265671ae868b4c96877
Taner <tanerozguner@hotmail.com>
2021-11-24 21:40:13 +0100

    V1.05
    Added library

--- frontend/src/app/components/rec-done/rec-done/rec-done.component.ts
+++ frontend/src/app/components/rec-done/rec-done/rec-done.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import WaveformPlaylist from 'waveform-playlist';
+
+
+
+
 
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
@@ -18,32 +23,71 @@ export class RecDoneComponent implements OnInit {
 
   ngOnInit(): void {
 
+    var playlist = WaveformPlaylist({
+      samplesPerPixel: 3000,
+      waveHeight: 70,
+      container: document.getElementById("playlist"),
+      state: "cursor",
+      isAutomaticScroll: true,
+
+      colors: {
+        waveOutlineColor: "#E0EFF1",
+        timeColor: "orange",
+        fadeColor: "orange"
+      },
+
+      controls: {
+        show: false,
+        width: 150,
+      },
+      zoomLevels: [500, 1000, 3000, 5000]
+    });
+
+
+    playlist.load([
+      {
+        src: "../../assets/audio/creative_minds.mp3"
+      }
+
+
+    ]).then(function () {
+      var ee = playlist.getEventEmitter();
+      document.getElementById("playAudio").addEventListener("click", function () {
+        ee.emit("play");
+        console.log("play")
+
+      });
+
+      document.getElementById("pauseAudio").addEventListener("click", function () {
+        ee.emit("pause");
+      });
+    });
+
 
   }
   open(content: any) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
 
     }, (reason) => {
-
     });
   }
 
-
-playSound(){
-  let audio = new Audio();
-  audio.src = "../assets/audio/creative_minds.mp3";
-  audio.load();
-  audio.play();
-
-}
-
-stopSound(){
-  let audio = new Audio();
-
-  audio.pause();
-  audio.currentTime = 0;
-
-}
+//
+// playSound(){
+//   let audio = new Audio();
+//   audio.src = "../assets/audio/creative_minds.mp3";
+//   audio.load();
+//   audio.play();
+//
+// }
+//
+// stopSound(){
+//   let audio = new Audio();
+//
+//   audio.pause();
+//   audio.currentTime = 0;
+//
+// }
 
 activateSoundWaves(){
   const soundWaves = document.getElementById("soundwavesWrapper");
