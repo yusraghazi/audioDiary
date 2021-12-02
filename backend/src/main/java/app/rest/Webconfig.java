31f34066e912bab2b3de88f23cb8435e27035992
H.R. Toenbreker <hanna.toenbreker@hva.nl>
2021-12-02 21:17:31 +0100

    attempt on error fix

--- backend/src/main/java/app/rest/Webconfig.java
+++ backend/src/main/java/app/rest/Webconfig.java
@@ -7,10 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Webconfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:4200", "http://localhost:8080");
         registry.addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT","DELETE")
-                .allowedOrigins("*");
+                .allowedOrigins("http://localhost:4200", "http://localhost:8080");
+//        registry.addMapping("/**")
+//                .allowedMethods("GET", "POST", "PUT","DELETE")
+//                .allowedOrigins("*");
     }
 }
