fcb5a1114a67f5d3766bf4947f938259d937c42a
H.R. Toenbreker <hanna.toenbreker@hva.nl>
2021-12-02 20:03:20 +0100

    really the last try to fix backend in deployment

--- backend/src/main/java/app/rest/Webconfig.java
+++ backend/src/main/java/app/rest/Webconfig.java
@@ -7,7 +7,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Webconfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:4200", "http://localhost:8080");
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://localhost:8080");
+                .allowedMethods("GET", "POST", "PUT","DELETE")
+                .allowedOrigins("*");
     }
 }
