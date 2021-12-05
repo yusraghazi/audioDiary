ba1b5037cc02dbd97a973e1ce702dcedceaa8466
H.R. Toenbreker <hanna.toenbreker@hva.nl>
2021-12-05 14:32:01 +0100

    changed services from hardcoded code to environment apiUrl

--- backend/src/main/java/app/rest/Webconfig.java
+++ backend/src/main/java/app/rest/Webconfig.java
@@ -9,8 +9,5 @@ public class Webconfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200", "http://localhost:8097");
-//        registry.addMapping("/**")
-//                .allowedMethods("GET", "POST", "PUT","DELETE")
-//                .allowedOrigins("*");
     }
 }
