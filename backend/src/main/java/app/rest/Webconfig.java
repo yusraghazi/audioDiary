ec042361c1d6775cc524c583fef6937c9e10b497
H.R. Toenbreker <hanna.toenbreker@hva.nl>
2021-12-06 11:35:11 +0100

    cors mapping issue

--- backend/src/main/java/app/rest/Webconfig.java
+++ backend/src/main/java/app/rest/Webconfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class Webconfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://localhost:8097");
+        registry.addMapping("/**");
     }
 }
