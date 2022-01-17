78660ad735cca728df1f1e00cf78f81d921aeefd
Taner <tanerozguner@hotmail.com>
2022-01-17 16:25:42 +0100

    Fixed it should create login test

--- frontend/src/app/components/forms/login/login.component.spec.ts
+++ frontend/src/app/components/forms/login/login.component.spec.ts
@@ -1,13 +1,33 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {MatDialogModule, MatDialogRef} from "@angular/material/dialog";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import {RouterModule} from "@angular/router";
+import {FormGroup, FormsModule, NgForm, ReactiveFormsModule} from "@angular/forms";
+import {AuthService} from "../../../services/auth.service";
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let componentHtml: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+        imports: [
+            FormsModule,
+            MatDialogModule,
+            HttpClientTestingModule,
+            RouterTestingModule,
+            RouterModule.forRoot([]),
+            ReactiveFormsModule],
+        providers: [
+            {
+                provide: MatDialogRef,
+                useValue: {}
+            },
+        ],
       declarations: [ LoginComponent ]
     })
     .compileComponents();
@@ -17,9 +37,12 @@ describe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    componentHtml = fixture.debugElement.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+
 });
