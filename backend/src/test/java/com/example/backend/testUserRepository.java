63032892c12e245e29e38c31d508c88d338e6dff
Taner <tanerozguner@hotmail.com>
2022-01-17 20:14:03 +0100

    Fixed workspace

--- backend/src/test/java/com/example/backend/testUserRepository.java
+++ backend/src/test/java/com/example/backend/testUserRepository.java
@@ -13,7 +13,6 @@ import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.Date;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
